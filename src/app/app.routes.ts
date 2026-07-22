1058f4b89912ded3a3b204bcb877d786ce4e80e6
Camila Pinheiro <camilafofinha012@gmail.com>
2026-07-22 16:38:14 -0300

    feat(router): adiciona authGuard na rota do carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,46 @@
+// import { Routes } from '@angular/router';
+// import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// import { Home } from './features/home/home/home';
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     component: Home,
+//   },
+//   {
+//     path: 'produtos',
+//     loadComponent: () =>
+//       import('./features/produtos/lista-produtos/lista-produtos').then(
+//         (m) => m.ListaProdutos
+//       ),
+//   },
+//   {
+//     path: 'carrinho',
+//     component: Carrinho,
+//   },
+// ];
+
+//! codigo final LazyLoading e LoadComponent
 import { Routes } from '@angular/router';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
-import { Home } from './features/home/home/home';
+import { authGuard } from './core/auth.guard';
 export const routes: Routes = [
   {
-    path: '',
-    component: Home,
+    path:'',
+    loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
   },
+
   {
     path: 'produtos',
-    loadComponent: () =>
-      import('./features/produtos/lista-produtos/lista-produtos').then(
-        (m) => m.ListaProdutos
-      ),
+    loadComponent: () => import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
   },
+
   {
     path: 'carrinho',
-    component: Carrinho,
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
