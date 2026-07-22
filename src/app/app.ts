e736f2f30c79f3fc7f4329534fc3703d00a83ecb
Camila Pinheiro <camilafofinha012@gmail.com>
2026-07-22 16:34:51 -0300

    feat(auth): disponibiliza estado de usuarioLogado e metodos de autenticacao no App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { usuarioLogado, login, logout } from './core/auth';
 // import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
@@ -11,4 +12,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 export class App {
   protected readonly title = signal('e-commerce-aluno');
   nomeLoja = 'LuNupi';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
