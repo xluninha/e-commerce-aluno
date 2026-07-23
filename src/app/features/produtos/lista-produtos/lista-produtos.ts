574d63362ef4a2327d59f84f6d22209507a2d542
Camila Pinheiro <camilafofinha012@gmail.com>
2026-07-23 14:03:29 -0300

    feat: implementa carregamento de produtos e estados com signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -2,6 +2,7 @@ import { Component, effect, signal, computed } from '@angular/core';
 import { Produto } from '../produto/produto';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { UpperCasePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -10,13 +11,37 @@ import { UpperCasePipe } from '@angular/common';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+
+  //! remover a lista de produtos, dados carregados via API Fakestoreapi
   produtos = signal <
   { nome: string ; preco: number } []> ([]);
-  
+//? criar estado de carregamentoo, 
+//** true: requisição em andamento, exibir indicador no template
+//! false: esconder indicador e exibir a lista de produtos
   carregando = signal(true);
-
   produtoSelecionado = signal<string | null>(null);
   carrinho = signal<{ nome: string; valor: number }[]>([]);
+
+   carregarProdutos() {
+    this.carregando.set(true);
+    this.http.get<
+      { title: string; valor: number }[]
+    >
+      ('https://fakstore.com/products')
+      .subscribe({
+        next: (dados) => {
+          const produtosFormatados = dados.map(p => ({
+            nome: p.title, preco: p.valor
+          }));
+          this.produtos.set(produtosFormatados);
+          this.carregando.set(false);
+        },
+        error: (e) => {
+          console.error('Erro ao carregar produtos: ', e);
+          this.carregando.set(false);
+        }
+      })
+  }
 
   // Computeds
   totalProdutos = computed(() => this.produtos().length);
@@ -25,7 +50,8 @@ export class ListaProdutos {
   totalCarrinho = computed(() => this.carrinho().reduce((total, item) => total + item.valor, 0));
 
   // O construtor correto em inglês
-  constructor() {
+  //! injetar httpClient dentro de construct, reestruturar constructor!!!
+  constructor( private http: HttpClient ) {
     effect(() => {
       console.log("Lista de Produtos Alterados: ", this.produtos());
     });
