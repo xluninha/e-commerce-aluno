e6999624eb10d51c63f49feedb5216cc833ec97d
Camila Pinheiro <camilafofinha012@gmail.com>
2026-08-14 15:03:50 -0300

    fix(interceptor): ajusta manipulacao de requisicoes http

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,13 +1,15 @@
 import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { tap, catchError, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
 const authService = inject(AuthService);
+// Router usado p redirecionamentos em erros de autenticação/autorização.
+const router = inject(Router);
 const token = authService.obterToken();
-// LOG REQUEST
 console.log('REQUEST', req.url);
-// TOKEN
+
 const novaReq = token
 ? req.clone({
 setHeaders: {
@@ -15,8 +17,6 @@ Authorization: `Bearer ${token}`,
 },
 })
 : req;
-
-//! SEGUE COM A NOVA REQUEST + LOG RESPONSE
 return next(novaReq).pipe(
 tap({
 next: (event) => console.log('RESPONSE:', event),
@@ -24,9 +24,18 @@ error: (error) => console.error('ERRO:', error),
 }),
 catchError((error) => {
 console.error('ERRO GLOBAL:', error);
+// 401 -> ausência de autenticação ou token inválido.
 if (error.status === 401) {
-console.warn('Não autorizado!');
+console.warn('Não autorizado. Faça login novamente.');
+authService.logout();
+router.navigateByUrl('/login');
 }
+// 403 -> usuario autenticado, mas sem permissão.
+if (error.status === 403) {
+console.warn('Acesso proibido. Perfil sem permissão.');
+router.navigateByUrl('/produtos');
+}
+// 500 -> erro interno do servidor
 if (error.status === 500) {
 console.warn('Erro interno do servidor!');
 }
