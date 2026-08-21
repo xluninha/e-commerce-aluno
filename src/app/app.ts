d0f7c3dbd49ce5e813b109aa9e6815a49e6e8254
Camila Pinheiro <Camilafofinha012@gmail.com>
2026-08-21 14:38:19 -0300

    refactor(app): ajustar inicialização do componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
+//? Este é o componente principal da sua aplicação. Ele junta a lógica com o HTML e CSS.
+
+// Importação de componentes
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
+  
+  // Componentes e recursos que este arquivo precisa usar no HTML dele
   imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  // Cria uma variável reativa do Angular (Signal) com o nome interno do projeto
   protected readonly title = signal('e-commerce-aluno');
+  // Variável comum que guarda o nome público da sua loja
   nomeLoja = 'Lunupi';
 }
