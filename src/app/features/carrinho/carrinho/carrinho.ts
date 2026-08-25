176d2d95f584d4c920b2c448d7d31e303b413402
Camila Pinheiro <camilafofinha012@gmail.com>
2026-08-25 14:49:48 -0300

    feat(carrinho): atualiza layout e logica da pagina de carrinho

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,16 +1,22 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 @Component({
 selector: 'app-carrinho',
-imports: [RouterLink, MatButtonModule],
+imports: [RouterLink, MatButtonModule, PrecoFormatadoPipe],
 templateUrl: './carrinho.html',
 styleUrl: './carrinho.css',
 })
 export class Carrinho {
+ private router = inject(Router);
+  public carrinhoFacade = inject(CarrinhoFacade);
+  private authFacade = inject(AuthFacade);
+
 // A página do carrinho passa a consumir o estado global do carrinho.
-carrinhoFacade = inject(CarrinhoFacade);
+CarrinhoFacade = inject(CarrinhoFacade);
 removerItem(indice: number) {
 // Remove um item específico da lista.
 this.carrinhoFacade.removerItem(indice);
@@ -19,4 +25,9 @@ limparCarrinho() {
 // Limpa todos os itens do carrinho.
 this.carrinhoFacade.limparCarrinho();
 }
+  cancelarCompra(){
+    this.carrinhoFacade.limparCarrinho();   
+    this.authFacade.sair();
+    this.router.navigate(['/login']);
+  }
 }
